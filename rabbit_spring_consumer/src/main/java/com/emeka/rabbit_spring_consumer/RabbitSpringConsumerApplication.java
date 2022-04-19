02e81e0f3426af34e0ff969e4cb42ee13e44c387
chukwudozie <emkychuks062@gmail.com>
2022-04-19 10:10:01 +0100

    add consumer

---
+++ rabbit_spring_consumer/src/main/java/com/emeka/rabbit_spring_consumer/RabbitSpringConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.emeka.rabbit_spring_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitSpringConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitSpringConsumerApplication.class, args);
+    }
+
+}
---
+++ rabbit_spring_consumer/src/test/java/com/emeka/rabbit_spring_consumer/RabbitSpringConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emeka.rabbit_spring_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitSpringConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
